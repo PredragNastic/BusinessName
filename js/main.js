e23ba007a04bb4a1e5c842e277b1e599cfa39590
Predrag Nastic <62741851+PredragNastic@users.noreply.github.com>
2022-03-07 13:37:10 +0100

    html, js
    made portfolio nav functional

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 //---UTILITY FUNCTIONS:
 const getsInputData = (id) => document.getElementById(id).value.trim();
-const setsAttribute = (id, att, val) => document.getElementById(id).setAttribute(att, val);
-const setsInnerText = (id, txt) => document.getElementById(id).innerText = txt;
 const clearsInputField = (id, val) => document.getElementById(id).value = val;
+const setsInnerText = (id, txt) => document.getElementById(id).innerText = txt;
+const setsAttribute = (id, att, val) => document.getElementById(id).setAttribute(att, val);
 const addsClass = (id, className) => document.getElementById(id).classList.add(className);
 const removesClass = (id, className) => document.getElementById(id).classList.remove(className);
+
+//---HEADER BACKGROUND ANIMATION:
+function headerBackgroundAnimation() {
+    let currentScrollPosition = window.scrollY;
+
+    (currentScrollPosition > 100) ? setsAttribute('page-top', "style", "background-color: #000; transition: 0.3s linear;")
+        : setsAttribute('page-top', "style", "background-color: transparent;");
+};
 
 //---SUBSCRIBE FORM VALIDATOR:
 function validatesSubscribeForm() {
@@ -23,15 +31,78 @@ function validatesSubscribeForm() {
     if (errorMessage.length === 0) clearsInputField('sub-mail-input', '');
 };
 
-//---HEADER BACKGROUND ANIMATION:
-function headerBackgroundAnimation() {
-    let currentScrollPosition = window.scrollY;
+//---PRTFOLIO NAVIGATION:
+function displayesAllProjects() {
+    removesClass('web-item-1', "d-none"), removesClass('web-item-2', "d-none"), removesClass('web-item-1', "mx-3"), removesClass('web-item-2', "mx-3");
+    removesClass('wordpress-item-1', "d-none"), removesClass('wordpress-item-2', "d-none"), removesClass('wordpress-item-1', "mx-3"), removesClass('wordpress-item-2', "mx-3");
+    removesClass('logo-design-item-1', "d-none"), removesClass('logo-design-item-2', "d-none"), removesClass('logo-design-item-1', "mx-3"), removesClass('logo-design-item-2', "mx-3");
+    removesClass('e-commerce-item-1', "d-none"), removesClass('e-commerce-item-2', "d-none"), removesClass('e-commerce-item-1', "mx-3"), removesClass('e-commerce-item-2', "mx-3");
+}
 
-    (currentScrollPosition > 100) ? setsAttribute('page-top', "style", "background-color: #000; transition: 0.3s linear;")
-        : setsAttribute('page-top', "style", "background-color: transparent;");
-};
+function displayesWebProjects() {
+    addsClass('wordpress-item-1', "d-none"), addsClass('wordpress-item-2', "d-none");
+    addsClass('logo-design-item-1', "d-none"), addsClass('logo-design-item-2', "d-none");
+    addsClass('e-commerce-item-1', "d-none"), addsClass('e-commerce-item-2', "d-none");
+
+    addsClass('portfolio-wrapper', "justify-content-center");
+    removesClass('web-item-1', "d-none"), removesClass('web-item-2', "d-none"), addsClass('web-item-1', "mx-3"), addsClass('web-item-2', "mx-3");
+}
+
+function displayesLogoDesignProjects() {
+    addsClass('web-item-1', "d-none"), addsClass('web-item-2', "d-none");
+    addsClass('wordpress-item-1', "d-none"), addsClass('wordpress-item-2', "d-none");
+    addsClass('e-commerce-item-1', "d-none"), addsClass('e-commerce-item-2', "d-none");
+
+    addsClass('portfolio-wrapper', "justify-content-center");
+    removesClass('logo-design-item-1', "d-none"), removesClass('logo-design-item-2', "d-none"), addsClass('logo-design-item-1', "mx-3"), addsClass('logo-design-item-2', "mx-3");
+}
+
+function displayesWordpressProjects() {
+    addsClass('web-item-1', "d-none"), addsClass('web-item-2', "d-none");
+    addsClass('logo-design-item-1', "d-none"), addsClass('logo-design-item-2', "d-none");
+    addsClass('e-commerce-item-1', "d-none"), addsClass('e-commerce-item-2', "d-none");
+
+    addsClass('portfolio-wrapper', "justify-content-center");
+    removesClass('wordpress-item-1', "d-none"), removesClass('wordpress-item-2', "d-none"), addsClass('wordpress-item-1', "mx-3"), addsClass('wordpress-item-2', "mx-3");
+}
+
+function displayesEcommerceProjects() {
+    addsClass('web-item-1', "d-none"), addsClass('web-item-2', "d-none");
+    addsClass('logo-design-item-1', "d-none"), addsClass('logo-design-item-2', "d-none");
+    addsClass('wordpress-item-1', "d-none"), addsClass('wordpress-item-2', "d-none");
+
+    addsClass('portfolio-wrapper', "justify-content-center");
+    removesClass('e-commerce-item-1', "d-none"), removesClass('e-commerce-item-2', "d-none"), addsClass('e-commerce-item-1', "mx-3"), addsClass('e-commerce-item-2', "mx-3");
+}
 
 //---EVENTS:
-window.addEventListener('scroll', () => headerBackgroundAnimation());
+//--Onaload:
 window.addEventListener('load', () => headerBackgroundAnimation());
+document.addEventListener('load', () => displayesAllProjects());
+
+//--Scroll:
+window.addEventListener('scroll', () => headerBackgroundAnimation());
+
+//--Clicks:
+document.getElementById('all-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    displayesAllProjects();
+});
+document.getElementById('web-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    displayesWebProjects();
+});
+document.getElementById('logo-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    displayesLogoDesignProjects();
+});
+document.getElementById('wp-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    displayesWordpressProjects();
+});
+document.getElementById('ecom-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    displayesEcommerceProjects();
+});
+
 document.getElementById('button-addon2').addEventListener('click', () => validatesSubscribeForm());
